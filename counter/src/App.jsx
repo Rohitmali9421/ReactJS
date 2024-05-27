af4a920d59f23253f12cf18530a8231d1253ac0d
Rohit Mali <rohitsmali9421@gmail.com>
2024-05-27 22:53:48 +0530

    counter project

---
+++ counter/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const increase =()=>{
+    setCount(count+1)
+  }
+  const decrease =()=>{
+    setCount(count-1)
+  }
+  return (
+    <>
+     <h1>count = {count}</h1>
+     <button onClick={increase}>Increase value</button>
+     <br />
+     <button onClick={decrease}>decrease value</button>
+    </>
+  )
+}
+
+export default App
