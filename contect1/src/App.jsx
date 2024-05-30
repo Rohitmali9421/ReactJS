e3ffba797521893302f327961c96499ca8f32f5b
Rohit Mali <rohitsmali9421@gmail.com>
2024-05-30 17:00:35 +0530

    Context 2 added

--- contect1/src/App.jsx
+++ contect1/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import Login from './Components/Login'
 import Profile from './Components/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <UserContextProvider>
