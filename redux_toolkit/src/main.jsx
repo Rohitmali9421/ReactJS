a0949e0d6a4b7395b3eaa2392486916370acca94
Rohit <Rohitsmali9421@gmail.com>
2025-01-09 12:48:21 +0530

    Learned Redux

--- redux_toolkit/src/main.jsx
+++ redux_toolkit/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {Provider} from "react-redux"
-import { store } from './app/store.js'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>,
+  </Provider>
 )
